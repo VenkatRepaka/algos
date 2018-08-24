e1f370307ad13d9e85acc21ebd2cb46925ae915d
Venkat Repaka <venkat.repaka@gmail.com>
2018-08-24 22:41:44 +0530

    First commit

--- src/strings/InfixToPostFix.java
+++ src/strings/InfixToPostFix.java
@@ -25,20 +25,45 @@ public class InfixToPostFix {
 
     public String infixToPostfix(String input) {
         Stack<Character> stack = new Stack<>();
+        String result = "";
         char currChar;
         for(int i=0;i<input.length();i++) {
-            currChar = input.charAt();
-            if(Character.isLetterOrDigit()) {
+            currChar = input.charAt(i);
+            if(Character.isLetterOrDigit(currChar)) {
+                result += currChar;
+            }
+            else if(currChar == '(') {
+                stack.push(currChar);
+            }
+            else if(currChar == ')') {
+                while (!stack.isEmpty() && stack.peek() != '(') {
+                    result += stack.pop();
+                }
+                if(!stack.isEmpty() && stack.peek() != '(') {
 
+                }
+                else {
+                    stack.pop();
+                }
+            }
+            else {
+                while (!stack.isEmpty() && precedence(currChar) <= precedence(stack.peek())) {
+                    result += stack.pop();
+                }
+                stack.push(currChar);
             }
         }
-        return null;
+        while (!stack.isEmpty()) {
+            result += stack.pop();
+        }
+        return result;
     }
 
     public static void main(String[] args) {
         InfixToPostFix infixToPostFix = new InfixToPostFix();
-        infixToPostFix.infixToPostfix("a+b*c+d"); // abc*+d+
-        // infixToPostFix.infixToPostfix("a+b*(c^d-e)^(f+g*h)-i");
+        // String postFix = infixToPostFix.infixToPostfix("a+b*c+d"); // abc*+d+
+        String postFix = infixToPostFix.infixToPostfix("a+b*(c^d-e)^(f+g*h)-i");
+        System.out.println(postFix);
     }
 
 
