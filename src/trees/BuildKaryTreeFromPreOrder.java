f8144b4e2e39aa93d89daaf33206967dbe371ca9
Venkat Repaka <venkat.repaka@gmail.com>
2018-09-26 02:51:00 +0530

    KaryTree from pre order

--- src/trees/BuildKaryTreeFromPreOrder.java
+++ src/trees/BuildKaryTreeFromPreOrder.java
@@ -1,5 +1,8 @@
 package trees;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /*
  This only works for height 3. Have to fix to figure out solution for any height.
  */
@@ -31,12 +34,14 @@ public class BuildKaryTreeFromPreOrder {
     private Index index = new Index();
 
     public static void main(String[] args) {
-        int[] preOrder = {1, 2, 5, 6, 7, 3, 8, 9, 10, 4};
+        // int[] preOrder = {1, 2, 5, 6, 7, 3, 8, 9, 10, 4};
+        // int[] preOrder = {1, 2, 5, 6, 7, 3, 8, 9, 10, 4, 11, 12, 13};
+        int[] preOrder = {1, 2, 5, 14, 15, 16, 6, 7, 3, 8, 9, 10, 4, 11, 12, 13};
         // int[] preOrder = {1, 2, 5, 6, 7, 3, 4};
         // int[] preOrder = {1, 2, 3, 4};
         BuildKaryTreeFromPreOrder buildKaryTreeFromPreOrder = new BuildKaryTreeFromPreOrder();
         int height = buildKaryTreeFromPreOrder.minHeightOfTree(preOrder.length, 3);
-        KaryTreeNode naryTree = buildKaryTreeFromPreOrder.buildKaryTree(preOrder, preOrder.length, 3, height, 1, height);
+        KaryTreeNode naryTree = buildKaryTreeFromPreOrder.buildKaryTree(preOrder, preOrder.length, 3, height, 1, new ArrayList<>());
         System.out.println(naryTree);
     }
 
@@ -51,27 +56,43 @@ public class BuildKaryTreeFromPreOrder {
         return ((int)Math.pow(k, height) - 1)/(k-1);
     }
 
-    public KaryTreeNode buildKaryTree(int[] pre, int n, int k, int height, int nodeNumber, int maxHeight) {
-        if (n==0) {
+    public KaryTreeNode buildKaryTree(int[] pre, int n, int k, int maxHeight, int depth, List<Integer> positional) {
+        if(index.index >= n) {
             return null;
         }
         KaryTreeNode newNode = new KaryTreeNode(k);
         newNode.data = pre[index.index];
-        int newNodeNumber = 0;
-        for (int i=0;i<k;i++) {
-            // if(index.index < n-1 && height > 1 && nodeNumber < n) {
-            int maxNumberOfNodesAtThisHeight = maxNoOfNodesCurrentHeight(maxHeight - height + 1, k);
-            newNodeNumber = maxNumberOfNodesAtThisHeight + 1 + i * (int)Math.pow(k, maxHeight - height);
-            // newNodeNumber = nodeNumber+(int)Math.pow(k, maxHeight-height)+i;
-            if(height > 1 && newNodeNumber < n) {
+        int maximumNoOfNodesCurrentDepth = maxNoOfNodesCurrentHeight(depth, k);
+        int totalNodes = maximumNoOfNodesCurrentDepth + childLastSize(positional, k);
+        for(int i=0;i<k;i++) {
+            if(depth < maxHeight && totalNodes <= n) {
                 index.index += 1;
-                newNode.child[i] = buildKaryTree(pre, n, k, height-1, newNodeNumber, maxHeight);
-            }
-            else {
-                newNode.setChild(null);
+                positional.add(i+1);
+                newNode.setChild(i, buildKaryTree(pre, n, k, maxHeight, depth + 1, positional));
+                positional.remove(positional.size()-1);
             }
         }
         return newNode;
+    }
+
+    private int childLastSize(List<Integer> pos, int k) {
+        int sum = 0;
+        for(int i=0;i<pos.size();i++) {
+            if(i == pos.size()-1) {
+                sum += k*pos.get(i);
+            }
+            else
+                /*if(pos.get(i) != 1) {
+                    sum += (int) Math.pow(k, pos.get(i));
+                }*/
+                if(pos.get(i) == 2) {
+                    sum += (int) Math.pow(k, pos.size() - i);
+                }
+                else if(pos.get(i) == 3) {
+                    sum += (int) Math.pow(k, pos.size() - i) * 3;
+                }
+        }
+        return sum;
     }
 
 }
