c0d28dd5bcebaab1d799cddc591aa5a2cdd02c19
Venkat Repaka <venkat.repaka@gmail.com>
2019-02-04 11:33:08 +0530

    Suffix Array Long

--- src/strings/coursera/week4/SuffixArrayLong.java
+++ src/strings/coursera/week4/SuffixArrayLong.java
@@ -2,8 +2,9 @@ package strings.coursera.week4;
 
 public class SuffixArrayLong {
 
-    public int[] countingSortChar(String text, int[] order) {
+    public int[] countingSortChar(String text) {
         int[] charArray = new int[200];
+        int[] order = new int[text.length()];
         for(int i=0;i<text.length();i++) {
             charArray[text.charAt(i)] += 1;
         }
@@ -43,19 +44,50 @@ public class SuffixArrayLong {
         }
         for (int i = text.length()-1; i >= 0; i--) {
             start = (order[i] - L + text.length()) % text.length();
-            System.out.print(start + " ");
+            // System.out.print(start + " ");
             class_ = sortedClass[start];
             count[class_] -= 1;
             newOrder[count[class_]] = start;
         }
-        System.out.println();
+        // System.out.println();
         return newOrder;
+    }
+
+    public int[] updateClasses(int[] newOrder, int[] computedClasses, int l) {
+        int n = newOrder.length;
+        int[] newClasses = new int[n];
+        int curr, prev, mid, midPrev;
+        for(int i=1;i<n;i++) {
+            curr = newOrder[i];
+            prev = newOrder[i-1];
+            mid = (curr + l)%n;
+            midPrev = (prev + l)%n;
+            if(computedClasses[curr] != computedClasses[prev] || computedClasses[mid] != computedClasses[midPrev]) {
+                newClasses[curr]= newClasses[prev] + 1;
+            }
+            else {
+                newClasses[curr]= newClasses[prev];
+            }
+        }
+        return newClasses;
+    }
+
+    public int[] buildSuffixArray(String text) {
+        int[] order = countingSortChar(text);
+        int[] computedClasses = computeClass(text, order);
+        int l = 1;
+        while(l < text.length()) {
+            order = sortDoubled(text, l, order, computedClasses);
+            computedClasses = updateClasses(order, computedClasses, l);
+            l *= 2;
+        }
+        return order;
     }
 
     public static void main(String[] args) {
         SuffixArrayLong suffixArrayMatching = new SuffixArrayLong();
-        String text = "ababaa$";
-        int[] order = suffixArrayMatching.countingSortChar(text, new int[text.length()]);
+        /*String text = "ababaa$";
+        int[] order = suffixArrayMatching.countingSortChar(text);
         for (int i = 0; i < order.length; i++) {
             System.out.print(order[i] + " ");
         }
@@ -68,6 +100,10 @@ public class SuffixArrayLong {
         int[] newOrder = suffixArrayMatching.sortDoubled(text, 2, order, computedClasses);
         for (int i = 0; i < newOrder.length; i++) {
             System.out.print(newOrder[i] + " ");
+        }*/
+        int[] order = suffixArrayMatching.buildSuffixArray("AACGATAGCGGTAGA$");
+        for (int i = 0; i < order.length; i++) {
+            System.out.print(order[i] + " ");
         }
     }
 
